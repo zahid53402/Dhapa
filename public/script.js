76d3de2fc5dfbfc49450d9da76f813ec2b7a649a
zahid53402 <zahid.ali.00347@gmail.com>
2026-03-13 23:37:29 +0500

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,3 +1,12 @@
+function openPair(){
+
+document.getElementById("home").style.display="none"
+
+document.getElementById("pair").style.display="block"
+
+}
+
+
 async function generate(){
 
 let number=document.getElementById("number").value
