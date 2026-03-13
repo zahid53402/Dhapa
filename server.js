cb117a9527b31b3d3a4053c9217672c7fcb6dee5
zahid53402 <zahid.ali.00347@gmail.com>
2026-03-13 23:34:20 +0500

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
-const express = require("express")
-const { default: makeWASocket, useMultiFileAuthState } = require("@whiskeysockets/baileys")
+const express=require("express")
 
-const app = express()
+const {
+default:makeWASocket,
+useMultiFileAuthState
+}=require("@whiskeysockets/baileys")
+
+const app=express()
 
 app.use(express.json())
 app.use(express.static("public"))
@@ -10,25 +14,25 @@ let sock
 
 async function start(){
 
-const { state, saveCreds } = await useMultiFileAuthState("./session")
+const {state,saveCreds}=await useMultiFileAuthState("./session")
 
-sock = makeWASocket({
-auth: state
+sock=makeWASocket({
+auth:state
 })
 
-sock.ev.on("creds.update", saveCreds)
+sock.ev.on("creds.update",saveCreds)
 
 }
 
 start()
 
-app.post("/pair", async (req,res)=>{
+app.post("/pair",async(req,res)=>{
 
 try{
 
-let number = req.body.number
+let number=req.body.number
 
-let code = await sock.requestPairingCode(number)
+let code=await sock.requestPairingCode(number)
 
 res.json({code:code})
 
@@ -40,8 +44,8 @@ res.json({error:e.message})
 
 })
 
-app.listen(process.env.PORT || 3000, ()=>{
+app.listen(process.env.PORT||3000,()=>{
 
-console.log("ZAHID KING SERVER STARTED")
+console.log("SERVER RUNNING")
 
 })
